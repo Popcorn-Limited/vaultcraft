305926b309d12d3d0368aea3612e531b7f1b3a89
RedVeil <leon.niesler@gmail.com>
2023-06-01 18:20:29 +0200

    added deposit limits

--- pages/create/limits.tsx
+++ pages/create/limits.tsx
@@ -1,11 +1,9 @@
 import MainActionButton from "@/components/buttons/MainActionButton";
 import SecondaryActionButton from "@/components/buttons/SecondaryActionButton";
 import ProgressBar from "@/components/ProgressBar";
-import AdapterSelection from "@/components/sections/AdapterSelection";
-import AssetSelection from "@/components/sections/AssetSelection";
-import ProtocolSelection from "@/components/sections/ProtocolSelection";
-import StrategySelection from "@/components/sections/StrategySelection";
+import DepositLimitConfiguration from "@/components/sections/DepositLimitConfiguration";
 import { CREATION_STAGES } from "@/lib/stages";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
 
 export default function Limits() {
@@ -15,28 +13,25 @@ export default function Limits() {
     <div className="bg-[#141416] md:max-w-[800px] w-full h-full flex flex-col justify-center mx-auto md:px-8 px-6">
       <ProgressBar stages={CREATION_STAGES} activeStage={3} />
       <div className="md:bg-[#23262F] self-center min-h-[500px] bg-transparent h-fit rounded-[20px] border-[#23262F] border-2 md:border border-none md:w-[600px] md:p-6 px-0 flex flex-col justify-between mt-10 md:relative w-full">
-        <h1 className="text-[white] text-2xl mb-2">Set up a new vault</h1>
+        <div>
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="text-white text-2xl mb-2">Deposit Limit</h1>
+            <p className="text-gray-500">Optional</p>
+          </div>
 
-        {/* @dev - We currently dont support naming vaults */}
-        {/* <label className="text-[white] mb-3">Name</label>
-                <Input
-                    onChange={(e) =>
-                        setBasicForm({ ...basicForm, name: (e.target as HTMLInputElement).value, })
-                    }
-                    value={basicForm.name}
-                    defaultValue={''}
-                    placeholder="Type vault name"
-                    autoComplete="off"
-                    autoCorrect="off"
-                    className={`
-                       border-1 border text-[white] border-[#353945] mb-4
-                    `}
-                /> */}
+          <div className="border-2 border-[#353945] rounded-lg flex flex-row justify-between w-full px-2 py-3 h-full bg-[#23262F] text-white mt-4">
+            <div className="flex flex-row">
+              <ExclamationCircleIcon className="text-white w-16 h-16 mr-4 pb-2" />
+              <p className="text-white text-sm">Deposits can be changed at any time after fund creation
+                Settings in this section are restrictive. Enable them to control who can deposit in your vault, and in what amounts.
+                If disabled, anyone can deposit any amount into your vault. </p>
+            </div>
+          </div>
 
-        <AssetSelection />
-        <ProtocolSelection />
-        <AdapterSelection />
-        <StrategySelection />
+          <div className="mt-6">
+            <DepositLimitConfiguration />
+          </div>
+        </div>
 
         <div className="flex flex-row space-x-8">
           <SecondaryActionButton label="Back" handleClick={() => router.push('/create/strategy')} />
