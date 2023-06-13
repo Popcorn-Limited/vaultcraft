4e498e6c84f7b19f20e86abcade20a7145e2e619
Nazar Shevchenko <shevanzr@gmail.com>
2023-06-13 11:14:41 +0300

    asset resolver curve

---
+++ lib/resolver/protocolAssets/resolver/curve.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.curve.fi/api/getPools"
+
+const ENDPOINTS = {
+    1: ["/ethereum/main", "/ethereum/crypto", "/ethereum/factory", "/ethereum/factory-crypto"],
+    10: ["/optimism/main", "/optimism/crypto", "/optimism/factory"],
+    137: ["/polygon/main", "/polygon/crypto", "/polygon/factory"],
+    250: ["/fantom/main", "/fantom/crypto", "/fantom/factory"],
+    1337: ["/ethereum/main", "/ethereum/crypto", "/ethereum/factory"],
+    42161: ["/arbitrum/main", "/arbitrum/crypto", "/arbitrum/factory"],
+    43114: ["/avalanche/main", "/avalanche/crypto", "/avalanche/factory"]
+} as { [chainId: number]: string[] }
+
+export async function curve({ chainId }: { chainId: number }): Promise<string[]> {
+
+    const pools: {
+        gaugeAddress?: string
+        lpTokenAddress: string
+    }[] = []
+
+    for (const endpoint of ENDPOINTS[chainId] || []) {
+        const response = await axios.get(BASE_URL + endpoint)
+        const responseData = response?.data?.data?.poolData
+        pools.push(...responseData)
+    }
+
+    return pools.filter(pool => !!pool?.gaugeAddress).map(pool => pool.lpTokenAddress)
+}
