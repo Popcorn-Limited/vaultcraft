736d62a0cf707cded215807cabde82381dbc1fd5
Nazar Shevchenko <shevanzr@gmail.com>
2023-06-13 11:51:44 +0300

    fixed

--- lib/resolver/adapterDefaults/resolver/alpacaV2.ts
+++ lib/resolver/adapterDefaults/resolver/alpacaV2.ts
@@ -1,11 +1,8 @@
+import axios from "axios"
 import { constants } from "ethers";
 
-export async function alpacaV2({ chainId, address }: { chainId: number, address: string }) {
-    const { moneyMarket } = await fetch("https://api.github.com/repos/alpaca-finance/alpaca-v2-money-market/contents/.mainnet.json")
-        .then(res => res.json())
-        .then(res => JSON.parse(atob(res.content)))
-
-    const { markets } = moneyMarket as {
+type MoneyMarketResponse = {
+    moneyMarket: {
         markets: {
             debtToken: string
             token: string
@@ -13,8 +10,14 @@ export async function alpacaV2({ chainId, address }: { chainId: number, address:
             interestModel: string
         }[]
     }
+}
 
-    const matchingMarket = markets.find(market => market.token.toLowerCase() === address.toLowerCase())
+export async function alpacaV2({ chainId, address }: { chainId: number, address: string }) {
+    const { data } = await axios.get("https://api.github.com/repos/alpaca-finance/alpaca-v2-money-market/contents/.mainnet.json")
+
+    const { moneyMarket } = JSON.parse(atob(data.content)) as MoneyMarketResponse
+
+    const matchingMarket = moneyMarket.markets.find(market => market.token.toLowerCase() === address.toLowerCase())
 
     return [ matchingMarket?.ibToken || constants.AddressZero ]
 }
