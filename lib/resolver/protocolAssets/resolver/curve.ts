862fb7dff09ebffcbfd7f6933de7c6a537f81ba1
Nazar Shevchenko <shevanzr@gmail.com>
2023-06-13 12:16:51 +0300

    fixed

--- lib/resolver/protocolAssets/resolver/curve.ts
+++ lib/resolver/protocolAssets/resolver/curve.ts
@@ -13,7 +13,6 @@ const ENDPOINTS = {
 } as { [chainId: number]: string[] }
 
 export async function curve({ chainId }: { chainId: number }): Promise<string[]> {
-
     const pools = (await Promise.all(ENDPOINTS?.[chainId]?.map(async endpoint => {
         const response = await axios.get(BASE_URL + endpoint)
         const responseData = response?.data?.data?.poolData
