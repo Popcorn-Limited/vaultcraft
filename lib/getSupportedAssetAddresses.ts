28cc1079861d70d2d88001ad6154981f45a00776
RedVeil <leon.niesler@gmail.com>
2023-09-06 16:18:35 +0200

    map fetched addresses to lower case

--- lib/getSupportedAssetAddresses.ts
+++ lib/getSupportedAssetAddresses.ts
@@ -5,9 +5,8 @@ export default async function getSupportedAssetAddresses(chainId: number, protoc
   const applicableProtocols = protocols.filter((p) => p.chains.includes(chainId))
 
   const addressesByProtocol = await Promise.all(applicableProtocols.map(protocol => resolveProtocolAssets({ chainId, resolver: protocol.key })))
-
-  const result: { [key: string]: string[] } = { all: addressesByProtocol.flat() }
-  applicableProtocols.forEach((p, i) => result[p.key] = addressesByProtocol[i])
+  const result: { [key: string]: string[] } = { all: addressesByProtocol.flat().map(a => a?.toLowerCase()) }
+  applicableProtocols.forEach((p, i) => result[p.key] = addressesByProtocol[i].map(a => a?.toLowerCase()))
 
   return result
 }
