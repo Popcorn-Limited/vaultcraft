1542b84023fbfc086f5637ceeb56e16b5f21c762
RedVeil <leon.niesler@gmail.com>
2023-09-12 11:31:33 +0200

    fixed build

--- pages/pro/review.tsx
+++ pages/pro/review.tsx
@@ -1,14 +1,12 @@
-import { PRO_CREATION_STAGES } from "@/lib/stages";
+import { BASIC_CREATION_STAGES } from "@/lib/stages";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { useAtom } from "jotai";
-import { useAccount } from "wagmi";
+import { useAccount, useNetwork } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
-import { noOp } from "@/lib/helpers";
-import { useDeployVault } from "@/lib/vaults/useDeployVault";
-import { metadataAtom, adapterAtom, strategyDeploymentAtom, conditionsAtom } from "@/lib/atoms";
+import { metadataAtom, adapterAtom, strategyDeploymentAtom, conditionsAtom, adapterDeploymentAtom, feeAtom, limitAtom, assetAtom } from "@/lib/atoms";
 import { IpfsClient } from "@/lib/ipfsClient";
 import Review from "@/components/review/Review";
 import MainActionButton from "@/components/buttons/MainActionButton";
@@ -16,13 +14,14 @@ import SecondaryActionButton from "@/components/buttons/SecondaryActionButton";
 import Modal from "@/components/Modal";
 import VaultCreationContainer from "@/components/VaultCreationContainer";
 import { ethers } from "ethers";
-import { toast } from "react-hot-toast";
+import { deployVault } from "@/lib/vaults/deployVault";
 
 
 export default function ReviewPage(): JSX.Element {
   const router = useRouter();
-  const { address: account } = useAccount();
+  const { address: account, connector } = useAccount();
   const { openConnectModal } = useConnectModal();
+  const { chain } = useNetwork()
 
   const [adapter] = useAtom(adapterAtom);
   const [strategyData] = useAtom(strategyDeploymentAtom);
@@ -30,25 +29,34 @@ export default function ReviewPage(): JSX.Element {
   const [showModal, setShowModal] = useState(false);
   const [conditions] = useAtom(conditionsAtom);
 
-  const { write: deployVault = noOp, isLoading, isSuccess, isError } = useDeployVault();
+  const [asset] = useAtom(assetAtom);
+  const [adapterData] = useAtom(adapterDeploymentAtom);
+  const [fees] = useAtom(feeAtom);
+  const [limit] = useAtom(limitAtom);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function handleSubmit() {
     setShowModal(true);
-    uploadMetadata();
+    deploy();
   }
 
-  function uploadMetadata() {
+  function deploy() {
     IpfsClient.add(metadata.name, { name: metadata.name }).then(res => {
-      setMetadata((prefState) => { return { ...prefState, ipfsHash: res } })
-      toast.loading("Deploying Vault...")
-      deployVault();
+      setMetadata((prefState) => { return { ...prefState, ipfsHash: res } });
+      setIsLoading(true)
+      deployVault(chain, connector, fees, asset, limit, account, adapterData, strategyData, res).then(res => {
+        !!res ? setIsSuccess(true) : setIsError(true);
+        setIsLoading(false)
+      }
+      )
     });
-    // toast.loading("Deploying Vault...")
-    // deployVault();
   }
 
   return (metadata && adapter ?
-    <VaultCreationContainer activeStage={3} stages={PRO_CREATION_STAGES} >
+    <VaultCreationContainer activeStage={2} stages={BASIC_CREATION_STAGES} >
       <div>
         <h1 className="text-white text-2xl mb-2">Review</h1>
         <p className="text-white">
@@ -70,7 +78,7 @@ export default function ReviewPage(): JSX.Element {
       <div className="flex justify-end mt-8 gap-3">
         <SecondaryActionButton
           label="Back"
-          handleClick={() => router.push('/pro/fees')}
+          handleClick={() => router.push('/easy/fees')}
           className={`max-w-[150px]`}
         />
         <MainActionButton
