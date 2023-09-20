8abf5ad89deaa53a1612ac14db14f22906ed9f0e
RedVeil <leon.niesler@gmail.com>
2023-09-20 13:02:36 +0200

    changed yearns api endpoint

--- lib/resolver/adapterApy/resolver/yearn.ts
+++ lib/resolver/adapterApy/resolver/yearn.ts
@@ -1,5 +1,5 @@
 export async function yearn({ chainId, address }: { chainId: number, address: string }): Promise<number> {
-  const res = await (await fetch(`https://api.yearn.fi/v1/chains/${chainId}/vaults/all`)).json()
+  const res = await (await fetch(`https://api.yexporter.io/v1/chains/${chainId}/vaults/all`)).json()
   const vault = res.find((vault: any) => vault.token.address.toLowerCase() === address.toLowerCase())
   return vault === undefined ? 0 : vault.apy.net_apy * 100
 }
