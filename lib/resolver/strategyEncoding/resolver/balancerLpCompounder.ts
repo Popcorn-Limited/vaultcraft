492079d0ee3a210e747671ec3d75943a8e90752a
RedVeil <leon.niesler@gmail.com>
2023-10-05 12:54:40 +0200

    readded balancer and aura

--- lib/resolver/strategyEncoding/resolver/balancerLpCompounder.ts
+++ lib/resolver/strategyEncoding/resolver/balancerLpCompounder.ts
@@ -1,39 +1,25 @@
-import { RPC_URLS } from "@/lib/connectors";
-import { ADDRESS_ZERO, MAX_INT256, ZERO } from "@/lib/constants";
+import { MAX_INT256, MAX_UINT256, ZERO } from "@/lib/constants";
 import { balancerApiProxyCall } from "@/lib/external/balancer/router/call";
 import { BatchSwapStep } from "@/lib/external/balancer/router/interfaces";
-import { Address, createPublicClient, encodeAbiParameters, http, parseAbiParameters, parseUnits, stringToHex } from "viem";
-import { PublicClient, mainnet } from "wagmi";
+import { Address, encodeAbiParameters, getAddress, parseUnits } from "viem";
+import { PublicClient } from "wagmi";
 import { StrategyEncodingResolverParams } from "..";
 
 interface BalancerRoute {
   swaps: BatchSwapStep[];
-  assets: string[];
-  limits: string[]
+  assets: Address[];
+  limits: BigInt[]
 }
 
-interface RouteWithTypes {
-  route: BalancerRoute;
-  swapTypes: string[];
-  assetTypes: string[];
-  limitTypes: string[];
+const toAssetRoute: BalancerRoute = {
+  swaps: [],
+  assets: [],
+  limits: [],
 }
 
-const batchSwapStepType = ["bytes32", "uint256", "uint256", "uint256", "bytes"]
+const BALANCER_VAULT: { [key: number]: Address } = { 1: "0xBA12222222228d8Ba445958a75a0704d566BF2C8" }
 
-const toAssetRoute = [
-  stringToHex("", { size: 32 }), // toAssetRoute.swaps.poolId
-  0,  // toAssetRoute.swaps.assetInIndex
-  0,  // toAssetRoute.swaps.assetOutIndex
-  ADDRESS_ZERO, // toAssetRoute.swaps.amount
-  "0x",  // toAssetRoute.swaps.userData
-  ADDRESS_ZERO, // toAssetRoute.assets
-  ZERO.toString(), // toAssetRoute.limits
-]
-
-const BALANCER_VAULT = { 1: "0xBA12222222228d8Ba445958a75a0704d566BF2C8" }
-
-async function createRouteWithTypes(sellToken: Address, buyToken: Address, chainId: number, client: PublicClient, gasPrice: string | null | undefined): Promise<RouteWithTypes> {
+async function createRoute(sellToken: Address, buyToken: Address, chainId: number, client: PublicClient, gasPrice: string | null | undefined): Promise<BalancerRoute> {
   const decimals = await client.readContract({
     address: sellToken,
     abi: tokenDecimalsAbi,
@@ -52,70 +38,162 @@ async function createRouteWithTypes(sellToken: Address, buyToken: Address, chain
     swaps: res.swaps.map((swap, i) => {
       return {
         poolId: swap.poolId,
-        assetInIndex: swap.assetInIndex,
-        assetOutIndex: swap.assetOutIndex,
-        amount: i === 0 ? "1" : "0",
-        userData: swap.userData
+        assetInIndex: BigInt(String(swap.assetInIndex)),
+        assetOutIndex: BigInt(String(swap.assetOutIndex)),
+        amount: i === 0 ? BigInt("1") : ZERO,
+        userData: swap.userData === "0x" ? "" : swap.userData
       }
     }),
-    assets: res.tokenAddresses,
-    limits: res.tokenAddresses.map((address, i) => i < (res.tokenAddresses.length - 1) ? MAX_INT256.toString() : BigInt("-1").toString())
+    assets: res.tokenAddresses.map(address => getAddress(address)),
+    limits: res.tokenAddresses.map((address, i) => i < (res.tokenAddresses.length - 1) ? MAX_INT256 : BigInt("-1"))
   }
 
-  return {
-    route,
-    swapTypes: route.swaps.map(swap => batchSwapStepType).flat(),
-    assetTypes: route.assets.map(asset => "address"),
-    limitTypes: route.assets.map(asset => "int256")
-  }
+  return route
 }
 
 export async function balancerLpCompounder({ chainId, client, address, params }: StrategyEncodingResolverParams): Promise<string> {
-  const route1 = await createRouteWithTypes(params[0][0], params[2], chainId, client, parseUnits("1", 9).toString()) // TODO - fetch gas price dynamically
-
-  const values = [
-    ...route1.route.swaps.map(swap => Object.entries(swap).map(([key, value]) => value)).flat(), // toBaseAssetRoutes1.swaps
-    ...route1.route.assets, // toBaseAssetRoutes1.assets
-    ...route1.route.limits, // toBaseAssetRoutes1.limits
-  ]
-  const types = [
-    ...route1.swapTypes,
-    ...route1.assetTypes,
-    ...route1.limitTypes,
-  ]
+  const route1 = await createRoute(params[0][0], params[2], chainId, client, parseUnits("1", 9).toString()) // TODO - fetch gas price dynamically
+  const values = [route1]
 
   if (params[0].length == 2) {
-    const route2 = await createRouteWithTypes(params[0][0], params[2], chainId, client, parseUnits("1", 9).toString()) // TODO - fetch gas price dynamically
-
-    values.push(...route2.route.swaps.map(swap => Object.entries(swap).map(([key, value]) => value)).flat())
-    values.push(...route2.route.assets)
-    values.push(...route2.route.limits)
-
-    types.push(...route2.swapTypes)
-    types.push(...route2.assetTypes)
-    types.push(...route2.limitTypes)
+    const route2 = await createRoute(params[0][0], params[2], chainId, client, parseUnits("1", 9).toString()) // TODO - fetch gas price dynamically
+    values.push(route2)
   }
 
-  return encodeAbiParameters(parseAbiParameters(
-    String([
-      "address", // baseAsset
-      "address", // vault
-      ...types,
-      ...batchSwapStepType.flat(), // toAssetRoute.swaps
-      "address", // toAssetRoute.assets
-      "int256",  // toAssetRoute.limits
-      ...params[1].map((value: string) => "uint256"), // minTradeAmounts
-      "bytes", // optionalData
-    ])),
+  return encodeAbiParameters(encodeAbi,
     [
-      params[2], // baseAsset
-      // @ts-ignore
+      params[2] as Address, // baseAsset
       BALANCER_VAULT[chainId], // vault
-      ...values,
-      ...toAssetRoute,
-      ...params[1], // minTradeAmounts
-      encodeAbiParameters(parseAbiParameters("bytes32,uint8"), params[3])  // optionalData
+      // @ts-ignore
+      values, // toBaseAssetRoute
+      // @ts-ignore
+      toAssetRoute,
+      params[1].map((value: string) => "0" ? MAX_UINT256 : BigInt(value)), // minTradeAmounts
+      encodeAbiParameters(balancerOptionalDataAbi, params[3])
     ]);
 }
 
-const tokenDecimalsAbi = [{ "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }] as const
+const balancerOptionalDataAbi = [{ name: 'poolId', type: 'bytes32' }, { name: 'indexIn', type: 'uint8' }] as const
+
+const baseAssetAbi = { name: 'baseAsset', type: 'address' } as const
+const vaultAbi = { name: 'vault', type: 'address' } as const
+const toBaseAssetRouteAbi = {
+  "internalType": "struct BalancerRoute[]",
+  "name": "_toAssetRoute",
+  "type": "tuple[]",
+  "components": [
+    {
+      "components": [
+        {
+          "internalType": "bytes32",
+          "name": "poolId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "assetInIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "assetOutIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "userData",
+          "type": "bytes"
+        }
+      ],
+      "internalType": "struct BatchSwapStep[]",
+      "name": "swaps",
+      "type": "tuple[]"
+    },
+    {
+      "internalType": "contract IAsset[]",
+      "name": "assets",
+      "type": "address[]"
+    },
+    {
+      "internalType": "int256[]",
+      "name": "limits",
+      "type": "int256[]"
+    },
+  ]
+} as const;
+const toAssetRouteAbi = {
+  "internalType": "struct BalancerRoute",
+  "name": "_toAssetRoute",
+  "type": "tuple",
+  "components": [
+    {
+      "components": [
+        {
+          "internalType": "bytes32",
+          "name": "poolId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "assetInIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "assetOutIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "userData",
+          "type": "bytes"
+        }
+      ],
+      "internalType": "struct BatchSwapStep[]",
+      "name": "swaps",
+      "type": "tuple[]"
+    },
+    {
+      "internalType": "contract IAsset[]",
+      "name": "assets",
+      "type": "address[]"
+    },
+    {
+      "internalType": "int256[]",
+      "name": "limits",
+      "type": "int256[]"
+    },
+  ]
+} as const;
+const minAmountOutAbi = { name: 'numbers', type: 'uint256[]' } as const
+const optionalDataAbi = { name: 'optionalData', type: 'bytes' } as const
+const encodeAbi = [baseAssetAbi, vaultAbi, toBaseAssetRouteAbi, toAssetRouteAbi, minAmountOutAbi, optionalDataAbi] as const
+
+const tokenDecimalsAbi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }] as const
+
+
