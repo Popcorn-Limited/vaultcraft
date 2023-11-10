7e73bf4c7d664509ef130a4b1fe5aaecec6e359e
RedVeil <leon.niesler@gmail.com>
2023-11-10 14:00:21 +0100

    modal overlay background

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -24,7 +24,7 @@ export default function Modal({ visibility, children, title, classNames = "" }: 
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
