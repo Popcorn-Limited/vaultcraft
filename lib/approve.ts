7607c98ec3e497f19a4000b8bbf9b7f87f1e759b
RedVeil <leon.niesler@gmail.com>
2024-01-04 14:27:29 +0100

    renamed log

--- lib/approve.ts
+++ lib/approve.ts
@@ -30,7 +30,7 @@ export async function handleAllowance({ token, amount, account, spender, clients
     functionName: "allowance",
     args: [account, spender]
   })
-  console.log({ allowance: Number(allowance), amount, sufficient: Number(allowance) < amount })
+  console.log({ allowance: Number(allowance), amount, insufficient: Number(allowance) < amount })
   if (Number(allowance) === 0 || Number(allowance) < amount) {
     console.log("APPROVE")
     return approve({ address: token, account, spender, publicClient: clients.publicClient, walletClient: clients.walletClient })
