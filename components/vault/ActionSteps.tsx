1c83af2115758d7dc987e2e12eb72a5688b1f518
RedVeil <leon.niesler@gmail.com>
2024-01-30 14:11:31 +0800

    using arrows in action steps

--- components/vault/ActionSteps.tsx
+++ components/vault/ActionSteps.tsx
@@ -1,5 +1,5 @@
 import { ActionStep } from "@/lib/getActionSteps";
-import { XMarkIcon } from "@heroicons/react/24/outline"
+import { ArrowRightIcon, XMarkIcon } from "@heroicons/react/24/outline"
 
 function getStepColor(preFix: string, step: any): string {
   if (step.loading || step.success) return `${preFix}-[#DFFF1C]`
@@ -16,7 +16,7 @@ export default function ActionSteps({ steps, stepCounter }: ActionStepsProps): J
   return (
     <div className="flex flex-row items-center">
       {steps.map((step, i) =>
-        <div key={step.label} className="flex flex-row items-center">
+        <div key={step.label} className="flex flex-row items-center w-14 h-8">
           <div
             className={`w-8 h-8 rounded-full border leading-none flex justify-center items-center cursor-default bg-opacity-40
               ${i === stepCounter ? "border-[#DFFF1C] bg-[#DFFF1C]" : `${getStepColor("border", step)} ${getStepColor("bg", step)}`}`}
@@ -26,7 +26,7 @@ export default function ActionSteps({ steps, stepCounter }: ActionStepsProps): J
             {!step.loading && step.success && <img src="/images/icons/checkIconYellow.svg" className={`h-4 w-4`} />}
             {!step.loading && !step.error && !step.success && <div className={`rounded-full h-3 w-3 ${i === stepCounter ? "bg-[#DFFF1C]" : getStepColor("bg", step)}`} />}
           </div>
-          {step.step < steps.length && <p className={`mb-4 ${getStepColor("text", step)} leading-0`}>___</p>}
+          {step.step < steps.length && <ArrowRightIcon className={`w-6 h-4 ${getStepColor("text", step)}`} />}
         </div>
       )}
     </div>
