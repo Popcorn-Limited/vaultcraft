a2127934cc5c90ca327aff8f3d3514fd6fcf0f36
RedVeil <leon.niesler@gmail.com>
2024-03-12 14:38:26 +0100

    hide test op gauge

--- lib/gauges/getGauges.ts
+++ lib/gauges/getGauges.ts
@@ -2,8 +2,9 @@ import { Address, PublicClient, getAddress } from "viem";
 import { ADDRESS_ZERO, GaugeAbi, GaugeControllerAbi } from "@/lib/constants";
 
 const HIDDEN_GAUGES = [
-  "0x38098e3600665168eBE4d827D24D0416efC24799", // failed gauge deployment
-  "0xF4c8736c1cf9b03ccB09DA6e8A8312E75CA5B529", // test op root gauge
+  "0x38098e3600665168eBE4d827D24D0416efC24799", // Deployment script ran out of gas and somehow added a random address into the gauges which now breaks these calls
+  "0xF4c8736c1cf9b03ccB09DA6e8A8312E75CA5B529", // Failed Op Gauge Test
+  "0xb5DC74CBF45A53a7E0920885ed506D5B862B119D", // Test Op Gauge 
 ]
 
 export interface Gauge {
