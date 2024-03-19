27116529e69ae7d30448cbf3082c10e3ec755464
RedVeil <leon.niesler@gmail.com>
2024-03-19 18:10:04 +0100

    vaultron page live

--- pages/vaultron.tsx
+++ pages/vaultron.tsx
@@ -6,6 +6,8 @@ import NoSSR from "react-no-ssr";
 import { useEffect, useState } from "react";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import MainActionButton from "@/components/button/MainActionButton";
+import SecondaryActionButton from "@/components/button/SecondaryActionButton";
+import { useRouter } from "next/router";
 
 const LevelNameByValue: { [key: number]: string } = {
   1: "Bronze",
@@ -13,27 +15,43 @@ const LevelNameByValue: { [key: number]: string } = {
   3: "Gold"
 }
 
+const DEFAULT_VAULTRON: VaultronStats = {
+  level: 0,
+  xp: 0,
+  animation: "",
+  image: "",
+  tokenId: 0
+}
+
 interface VaultronStats {
   level: number;
   xp: number;
   animation: string;
+  image: string;
+  tokenId: number;
 }
 
 async function fetchVaultron(account: Address, client: PublicClient): Promise<VaultronStats> {
-  const tokenId = await client.readContract({
-    address: VAULTRON,
-    abi: VaultronAbi,
-    functionName: "getActiveTokenId",
-    args: [account]
-  })
-  const tokenURI = await client.readContract({
-    address: VAULTRON,
-    abi: VaultronAbi,
-    functionName: "tokenURI",
-    args: [tokenId]
-  })
-  const { data } = await axios.get(tokenURI)
-  return { level: Number(data.properties.Level.value), xp: Number(data.properties.XP.value), animation: data.animation_url }
+  try {
+    const tokenId = await client.readContract({
+      address: VAULTRON,
+      abi: VaultronAbi,
+      functionName: "getActiveTokenId",
+      args: [account]
+    })
+    const tokenURI = await client.readContract({
+      address: VAULTRON,
+      abi: VaultronAbi,
+      functionName: "tokenURI",
+      args: [tokenId]
+    })
+    const { data } = await axios.get(tokenURI)
+    console.log(data)
+    return { level: Number(data.properties.Level.value), xp: Number(data.properties.XP?.value || 0), animation: data.animation_url, image: data.image, tokenId: Number(tokenId) }
+  } catch (e) {
+    console.log(e)
+    return DEFAULT_VAULTRON
+  }
 }
 
 export default function Test() {
@@ -41,38 +59,71 @@ export default function Test() {
   const publicClient = usePublicClient({ chainId: 137 })
   const { openConnectModal } = useConnectModal();
 
-  const [vaultronStats, setVaultronStats] = useState<VaultronStats>({ level: 0, xp: 0, animation: "" })
+  const [vaultronStats, setVaultronStats] = useState<VaultronStats>(DEFAULT_VAULTRON)
 
   useEffect(() => {
     if (account) {
-      fetchVaultron("0xb6fBf1CC3a397815C7c00A83A2063782EAFD1036", publicClient)
+      fetchVaultron(account, publicClient)
         .then(res => setVaultronStats(res))
     }
   }, [account])
 
+  function handleMercle(tokenId: number) {
+    if (!window) return
+    if (tokenId === 0) {
+      // @ts-ignore
+      window.open("https://app.mercle.xyz/vaultcraft/events", '_blank').focus()
+    } else {
+      // @ts-ignore
+      window.open("https://app.mercle.xyz/vaultcraft/events/profile", '_blank').focus()
+    }
+  }
+
+  function handleOpensea(tokenId: number) {
+    if (!window) return
+    if (tokenId === 0) {
+      // @ts-ignore
+      window.open("https://opensea.io/collection/vaultcraftvaultronnft", '_blank').focus()
+    } else {
+      // @ts-ignore
+      window.open(`https://opensea.io/assets/matic/0x590e3a9260ffb7887ffd54a57d1facf7db59c751/${tokenId}`, '_blank').focus()
+    }
+  }
 
   return (
     <NoSSR>
-      <section className="flex flex-row justify-between space-x-10 px-20 text-white">
-        <div className="w-2/3">
-          <img alt="Placeholder Vaultron" src="https://resolve.mercle.xyz/ipfs/bafkreibn26tzshouo6ayr33uhwwqzxpp5h6zgzitzgxwhsacsuuxoo7fuq" className="" />
+      <section className="md:flex md:flex-row w-full md:w-9/12 px-4 md:mx-auto text-white">
+        <div className="w-full md:w-2/3">
+          {vaultronStats.animation === "" ?
+            <img
+              alt="Placeholder Vaultron"
+              src="https://resolve.mercle.xyz/ipfs/bafkreibn26tzshouo6ayr33uhwwqzxpp5h6zgzitzgxwhsacsuuxoo7fuq"
+              className="w-[50rem] h-[50rem] rounded-2xl object-none blur-md"
+            />
+            :
+            <iframe
+              src={vaultronStats.animation}
+              className="w-full h-84 rounded-2xl md:w-[50rem] md:h-[50rem]"
+            />
+          }
         </div>
-        <div className="w-1/3">
-          <h1 className="text-4xl font-bold">
-            {LevelNameByValue[vaultronStats.level]} VAULTRON
+        <div className="w-full md:w-1/3 mt-8 md:mt-0">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            {LevelNameByValue[vaultronStats.level]} Vaultron
           </h1>
 
-          <div className="w-full rounded-full border-white py-0.5 px-4">
-            <div className="">
-              My NFT
-            </div>
+          <div className="w-full flex flex-row items-center justify-between rounded-full border border-white mt-2 py-2 px-2">
+            <VaultronLevel label="None" active={vaultronStats.level === 0} />
+            <VaultronLevel label="Bronze" active={vaultronStats.level === 1} />
+            <VaultronLevel label="Silver" active={vaultronStats.level === 2} />
+            <VaultronLevel label="Gold" active={vaultronStats.level === 3} />
           </div>
 
-          <p>
-            Your Vaultron NFT gives you access to XP points, airdrops and oVCX rewards. <span>View More</span>
+          <p className="mt-16">
+            Your Vaultron NFT gives you access to XP points, airdrops and oVCX rewards.
           </p>
 
-          <div>
+          <div className="mt-16">
             <p className="text-sm text-gray-400">Your Points</p>
             {account ?
               <div className="space-y-4 mt-2">
@@ -85,6 +136,17 @@ export default function Test() {
               />
             }
           </div>
+
+          <div className="flex flex-row items-center space-x-4 rounded-2xl border border-[#353945] bg-[#23262f] p-6 mt-16">
+            <MainActionButton
+              label="View on Mercle"
+              handleClick={() => handleMercle(vaultronStats.tokenId)}
+            />
+            <SecondaryActionButton
+              label="View on Opensea"
+              handleClick={() => handleOpensea(vaultronStats.tokenId)}
+            />
+          </div>
         </div>
       </section>
     </NoSSR>
@@ -93,11 +155,11 @@ export default function Test() {
 
 
 function InfoRow({ label, value }: { label: string, value: number }): JSX.Element {
-  return <div className="flex flex-row items-center pb-0.5 border-b border-gray-600">
+  return <div className="flex flex-row items-center pb-1 border-b border-gray-600">
     <img
       src={"/images/tokens/vcx.svg"}
       alt="VCX"
-      className="w-6 md:w-12 h-6 md:h-12 object-contain rounded-full"
+      className="w-12 h-12 object-contain rounded-full mr-4"
     />
     <div>
       <p className="text-sm text-gray-400">
@@ -108,4 +170,12 @@ function InfoRow({ label, value }: { label: string, value: number }): JSX.Elemen
       </p>
     </div>
   </div>
+}
+
+function VaultronLevel({ label, active }: { label: string, active: boolean }): JSX.Element {
+  return (
+    <div className={`px-2 py-1 rounded-full w-20 text-center font-bold ${active ? "text-black bg-white" : "text-white"}`}>
+      <p className="mt-0.5">{label}</p>
+    </div>
+  )
 }
