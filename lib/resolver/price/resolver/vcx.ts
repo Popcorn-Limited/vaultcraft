977a056ad4e8c90d2c3ad52ce51ce3cc5b626bb1
RedVeil <leon.niesler@gmail.com>
2024-04-12 12:58:59 +0200

    vcx price quick fix

--- lib/resolver/price/resolver/vcx.ts
+++ lib/resolver/price/resolver/vcx.ts
@@ -6,7 +6,12 @@ export async function vcx({
   chainId,
   client,
 }: PriceResolverParams): Promise<number> {
-  const { data } = await axios.get("https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577a7f7ee659aa14dc16fd384b3f8078e23f1920000200000000000000000633-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2-0xcE246eEa10988C495B4A90a905Ee9237a0f91543");
+  try {
+    const { data } = await axios.get("https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577a7f7ee659aa14dc16fd384b3f8078e23f1920000200000000000000000633-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2-0xcE246eEa10988C495B4A90a905Ee9237a0f91543");
 
-  return Number(data.pair.priceUsd)
+    return Number(data.pair.priceUsd)
+  } catch(e){
+    console.log("error fetching vcx price: ",e)
+    return 0.09133
+  }
 }
