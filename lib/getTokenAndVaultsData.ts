0925b8e93d6c0eb1d9da2ba00bed87236f7ab479
RedVeil <leon.niesler@gmail.com>
2024-04-15 10:26:15 +0200

    fixed vcx price fetching

--- lib/getTokenAndVaultsData.ts
+++ lib/getTokenAndVaultsData.ts
@@ -260,8 +260,14 @@ async function prepareVaultsData(chainId: number, client: PublicClient): Promise
 }
 
 async function getVCXPrice(): Promise<number> {
-  const { data: vcxPriceRes } = await axios.get("https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577a7f7ee659aa14dc16fd384b3f8078e23f1920000200000000000000000633-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2-0xcE246eEa10988C495B4A90a905Ee9237a0f91543")
-  return Number(vcxPriceRes.pair.priceUsd)
+  try {
+    const { data } = await axios.get("https://api.dexscreener.com/latest/dex/pairs/ethereum/0x577a7f7ee659aa14dc16fd384b3f8078e23f1920000200000000000000000633-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2-0xcE246eEa10988C495B4A90a905Ee9237a0f91543");
+
+    return Number(data.pair.priceUsd)
+  } catch (e) {
+    console.log("error fetching vcx price: ", e)
+    return 0.08563
+  }
 }
 
 async function prepareAssets(addresses: Address[], chainId: number, client: PublicClient): Promise<TokenByAddress> {
