8de0cbf26013292fa013346b4a67327265a1346b
RedVeil <leon.niesler@gmail.com>
2024-04-23 11:42:54 +0200

    added ref link

--- components/navbar/NavbarLinks.tsx
+++ components/navbar/NavbarLinks.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import NavbarLink from "@/components/navbar/NavbarLink";
 import { isAddress } from "viem";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { useAccount } from "wagmi";
+import { showErrorToast, showSuccessToast } from "@/lib/toasts";
 
 const links: { label: string; url: string; onClick?: Function }[] = [
   {
@@ -30,23 +33,37 @@ const links: { label: string; url: string; onClick?: Function }[] = [
   {
     label: "Buy VCX",
     url: "https://swap.cow.fi/#/1/swap/WETH/VCX",
-  },
+  }
 ];
 
 export default function NavbarLinks(): JSX.Element {
   const router = useRouter();
   const { query } = router;
+  const { address: account } = useAccount();
 
   return (
     <>
-      {links.map((link) => (
-        <NavbarLink
-          key={link.label}
-          label={link.label}
-          url={(!!query?.ref && isAddress(query.ref as string)) ? `${link.url}?ref=${query.ref}` : `${link.url}`}
-          isActive={router.pathname === link.url}
-        />
-      ))}
+      {
+        links.map((link) => (
+          <NavbarLink
+            key={link.label}
+            label={link.label}
+            url={(!!query?.ref && isAddress(query.ref as string)) ? `${link.url}?ref=${query.ref}` : `${link.url}`}
+            isActive={router.pathname === link.url}
+          />
+        ))
+      }
+      <CopyToClipboard
+        text={`https://app.vaultcraft.io/vaults?ref=${account}`}
+        onCopy={() => account
+          ? showSuccessToast("Referal link copied!")
+          : showErrorToast("Connect your Wallet to copy referal link")
+        }
+      >
+        <p className="leading-5 text-lg text-black font-medium hover:font-bold transition-all ease-in-out delay-250 cursor-pointer">
+          Copy Referal link
+        </p>
+      </CopyToClipboard>
     </>
   );
 }
