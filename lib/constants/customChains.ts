a2c172f830f1ddbe9ca3974a6dfb3d41825a8c9d
RedVeil <leon.niesler@gmail.com>
2024-05-15 14:14:12 +0200

    added multicall3 for xLayer

--- lib/constants/customChains.ts
+++ lib/constants/customChains.ts
@@ -12,5 +12,8 @@ export const xLayer = {
   blockExplorers: {
     default: { name: 'Okx Exlporer', url: 'https://www.okx.com/web3/explorer/xlayer' },
   },
+  contracts: {
+    multicall3: { address: "0xca11bde05977b3631167028862be2a173976ca11", blockCreated: 47416 }
+  },
   testnet: false
 } as const satisfies Chain
