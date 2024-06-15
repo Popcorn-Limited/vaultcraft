6b863ab3dd317b9a36c3b811468a46b773c8a354
D3Portillo <d3portillo@gmail.com>
2024-06-15 19:45:17 +0200

    chores: VaultRow updates

--- components/vault/BoostVaultRow.tsx
+++ components/vault/BoostVaultRow.tsx
@@ -1,22 +1,22 @@
-import { type Address, isAddress } from "viem";
+import { type Address } from "viem";
 import { useAtom } from "jotai";
+import { useAccount } from "wagmi";
 
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
 import {
   NumberFormatter,
-  formatAndRoundNumber,
   formatTwoDecimals,
 } from "@/lib/utils/formatBigNumber";
 import { VaultLabel, type VaultData } from "@/lib/types";
 import { tokensAtom } from "@/lib/atoms";
 import { cn } from "@/lib/utils/helpers";
-import { useRouter } from "next/router";
 import AssetWithName from "./AssetWithName";
 import { Fragment, useState } from "react";
 import useGaugeWeights from "@/lib/gauges/useGaugeWeights";
-import { useAccount } from "wagmi";
+
+import TokenIcon from "../common/TokenIcon";
 import Badge from "./Badge";
 
 export default function BoostVaultRow({
@@ -64,6 +64,7 @@ export default function BoostVaultRow({
   const dataAsset = tokens[chainId]?.[asset] ?? {};
   const dataGauge = tokens[chainId]?.[gauge!] ?? {};
 
+  console.debug({ vault, dataAsset, dataGauge, vaultData });
   const GAUGE_ADDRESS = vaultData.gauge!;
 
   let boost = Math.round(maxGaugeApy / minGaugeApy) || 0;
@@ -112,7 +113,7 @@ export default function BoostVaultRow({
       >
         <td className="relative">
           <AssetWithName
-            className="[&_h2]:font-normal pl-3 [&_h2]:text-lg"
+            className="[&_h2]:font-normal relative hover:z-10 pl-3 [&_h2]:text-lg"
             vault={vaultData}
           />
         </td>
@@ -137,9 +138,29 @@ export default function BoostVaultRow({
           {(Number(weights?.[1]) / 1e16).toFixed(2) || 0}%
         </td>
 
-        <td className="text-right text-lg">2,500</td>
+        <td className="text-right text-lg">
+          <nav className="flex [&_img]:brightness-125 [&_img]:saturate-150 gap-2 items-center">
+            <TokenIcon
+              chainId={1}
+              token={{} as any}
+              icon="/images/tokens/oVcx.svg"
+              imageSize="w-6 h-6"
+            />
+            <span className="pt-0.5">2,500</span>
+          </nav>
+        </td>
 
-        <td className="text-right text-lg">3,5000</td>
+        <td className="text-right text-lg">
+          <nav className="flex [&_img]:brightness-125 [&_img]:saturate-150 gap-2 items-center">
+            <TokenIcon
+              chainId={1}
+              token={{} as any}
+              icon="/images/tokens/oVcx.svg"
+              imageSize="w-6 h-6"
+            />
+            <span className="pt-0.5">2,500</span>
+          </nav>
+        </td>
       </tr>
 
       <tr
@@ -173,16 +194,16 @@ export default function BoostVaultRow({
 
         <td colSpan={3} className="border-t border-customNeutral100">
           <nav className="flex items-center gap-4">
-            <span className="whitespace-nowrap min-w-[12rem]">
-              My Votes:{" "}
-              <strong className="text-lg ml-1">
+            <div className="flex items-center gap-2 whitespace-nowrap min-w-[12rem]">
+              <span>My Votes:</span>
+              <strong className="text-lg mr-1">
                 {(amount / 100).toFixed(2) || 0}%
               </strong>
-              <Badge className="ml-1">x{boost}</Badge>
-            </span>
+              <Badge>x{boost}</Badge>
+            </div>
 
             <span className="whitespace-nowrap">Vote %</span>
-            <fieldset className="select-none w-full flex items-center pr-2 pl-1">
+            <fieldset className="select-none w-full flex items-center pr-2">
               <Slider
                 railStyle={{
                   backgroundColor: isVotingAvailable ? "#FFFFFF" : "#AFAFAF",
