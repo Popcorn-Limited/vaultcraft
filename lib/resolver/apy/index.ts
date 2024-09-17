502d455ec5f2b714bd460dc6741d5d6e20c6fe4f
RedVeil <leon.niesler@gmail.com>
2024-09-17 08:19:36 +0200

    reduced maticX apy to 5

--- lib/resolver/apy/index.ts
+++ lib/resolver/apy/index.ts
@@ -75,7 +75,7 @@ export async function getLooperApy(address: Address, apyId: string, chainId: num
   const rewardApy = chainId === 1 ? 1.5 : 0
 
   const leverageApy = baseApy.map(entry => {
-    entry.apy = chainId === 137 ? 5.17 : entry.apy;
+    entry.apy = chainId === 137 ? 5 : entry.apy;
     const apyBase = entry.apy + ((entry.apy - borrowRate) * (leveragRatio - 1));
     const apyReward = (entry.apyReward + rewardApy) * leveragRatio;
     return {
