6d4adde101aa1c900f93e161b669db1e0e147c00
RedVeil <leon.niesler@gmail.com>
2025-05-28 12:39:58 +0200

    added hemi vaultRouter

--- lib/constants/addresses.ts
+++ lib/constants/addresses.ts
@@ -49,7 +49,7 @@ export const AsyncRouterByChain: AddressByChain = {
   [avalanche.id]: "0x7902c757a4A4B44C3B271Ce2f0c2A575C6D5f57A",
   [polygon.id]: zeroAddress,
   [bsc.id]: zeroAddress,
-  [hemi.id]: zeroAddress,
+  [hemi.id]: "0x06C0c876419a76E89AD55D1225bB335939C25150",
   [morph.id]: "0x06C0c876419a76E89AD55D1225bB335939C25150",
 }
 
